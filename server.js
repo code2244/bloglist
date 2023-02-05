28ccc18f54e39c2866b971eb6ebf89fd1a60a76e
Ricardo Hernandez <rmhm.photography@gmail.com>
2023-02-04 21:07:19 -0400

    Structured project.

--- server.js
+++ server.js
@@ -1,65 +1,9 @@
-// dependencies 
-require('dotenv').config()
+const app = require('./app') // the actual express app
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// logger middleware
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-// Blog Schema
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number 
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-const Blog = mongoose.model('Blog', blogSchema)
-
-// connection to mongoDB
-
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
-// Middleware
-app.use(cors())
-app.use(bodyParser.json())
-app.use(requestLogger)
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
